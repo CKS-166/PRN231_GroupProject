078103cc83f6217392158739683528cd4e808aa7
SuongCao <caokhasuong2003@gmail.com>
2024-07-13 20:56:24 +0700

    initial commit

---
+++ FPTUStarterSolution/FPTU_Starter.API/Controllers/DemoController.cs
@@ -0,0 +1,52 @@
+﻿using FPTU_Starter.Domain.Constrain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FPTU_Starter.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        // GET: api/<DemoController>
+        [HttpGet]
+        [Authorize]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        [HttpGet("GetAdmin")]
+        [Authorize(Roles = Role.Admin)]
+        public IEnumerable<string> GetAdmin()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<DemoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DemoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DemoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DemoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
